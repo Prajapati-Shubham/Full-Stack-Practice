0da26cb551f3b5fc23a020650cbfbe4fcdedfbb2
Prajapati-Shubham <prajapatishubham575@gmail.com>
2023-12-30 23:12:34 +0530

    React Project Complete

--- MaterialUI/Mini-Project-React/src/InfoBox.jsx
+++ MaterialUI/Mini-Project-React/src/InfoBox.jsx
@@ -4,20 +4,26 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
 
 export default function InfoBox({info}){
     const IMG_ULR="https://images.unsplash.com/photo-1545134969-8debd725b007?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGR1c3R5JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+    const HOT_URL="https://images.unsplash.com/photo-1561553873-e8491a564fd0?q=80&w=947&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const COLD_URL="https://images.unsplash.com/photo-1425100599170-85ec4f00a6ee?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const RAIN_URL="https://images.unsplash.com/photo-1534274988757-a28bf1a57c17?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFpbnklMjB3ZWF0aGVyfGVufDB8fDB8fHww";
     return(
     <div className="infoBx">
             <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
-        image={IMG_ULR}
+        image={info.humidity>80?RAIN_URL:info.temp>15?HOT_URL:COLD_URL}
         title="green iguana"
       />
         <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-            {info.city}
+            {info.city}{info.humidity>80?<ThunderstormIcon/>:info.temp>15?<WbSunnyIcon/>:<AcUnitIcon/>}
             </Typography>
             <Typography variant="body2" color="text.secondary" component="span">
             <p>Temprature={info.temp}&deg;C</p>
